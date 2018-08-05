9d93adda935ee7d3e54fba91d849b00ed596e646
Christopher Schon <chris.g.schon@gmail.com>
2018-08-05 20:40:48 +0000

    Cookie cutter data science template generated from

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DrivenData competition submission repository',
+    author='Chris Schon',
+    license='MIT',
+)
