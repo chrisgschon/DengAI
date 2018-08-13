4bbc1251e7d6470f56d66678589c62e27d6b4fd1
Christopher Schon <chris.g.schon@gmail.com>
2018-08-13 18:09:42 +0100

    Model benchmarking and submission notebook development

--- archive/Feature_Engineering_archive.py
+++ archive/Feature_Engineering_archive.py
@@ -19,3 +19,88 @@ plt.plot(forecast_dates, forecast, c= 'blue', label = 'Forecast');
 plt.fill_between(forecast_dates, conf_int[:,0], conf_int[:,1],
     alpha=0.5, edgecolor='#1B2ACC', facecolor='#089FFF')
 plt.legend();
+
+pre_process = Pipeline([("imputer", Imputer(strategy="mean")), ("scaler", RobustScaler())])
+sj_prefit = pre_process.fit(train_sj[best_raw_features])
+iq_prefit = pre_process.fit(train_iq[best_raw_features])
+
+best_raw_features = ['reanalysis_dew_point_temp_k', 'reanalysis_min_air_temp_k', 'reanalysis_max_air_temp_k','reanalysis_specific_humidity_g_per_kg',
+                    'station_min_temp_c', 'reanalysis_tdtr_k', 'station_avg_temp_c']
+raw_features = numeric_cols
+
+from SKL_search import *
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.preprocessing import PolynomialFeatures
+
+num_pca = 3
+num_feature_pipeline = Pipeline([("imputer", Imputer(strategy="mean")),
+                    ('features', 
+                    FeatureUnion([('pca', PCAFeatureSelector(k = num_pca)), ('poly', PolynomialFeatures(interaction_only = False, include_bias = True))])), 
+                                 ("scaler", RobustScaler())])
+
+num_sj_pipe_fit = num_feature_pipeline.fit(train_sj[numeric_cols], train_sj['total_cases'])
+num_iq_pipe_fit = num_feature_pipeline.fit(train_iq[numeric_cols], train_iq['total_cases'])
+
+num_sj_train = num_feature_pipeline.fit_transform(train_sj[numeric_cols], train_sj['total_cases'])
+num_iq_train = num_feature_pipeline.fit_transform(train_iq[numeric_cols], train_iq['total_cases'])
+
+num_sj_test = num_sj_pipe_fit.transform(test_sj[numeric_cols])
+num_iq_test = num_iq_pipe_fit.transform(test_iq[numeric_cols])
+
+# Add month and quarter one hots
+
+from sklearn.preprocessing import OneHotEncoder
+enc = OneHotEncoder()
+cats_sj_train = enc.fit_transform(train_sj[categorical_features]).toarray()
+cats_sj_test = enc.fit_transform(test_sj[categorical_features]).toarray()
+cats_iq_train = enc.fit_transform(train_iq[categorical_features]).toarray()
+cats_iq_test = enc.fit_transform(test_iq[categorical_features]).toarray()
+
+from scipy import stats
+
+sj_feature_train = pd.DataFrame(X_sj)
+sj_feature_train['weekofyear'] = train_sj['weekofyear']
+sj_feature_train['poly_fit'] = train[train['city'] == 'sj']['poly_fit']
+sj_feature_train['total_cases'] = train_sj['total_cases']
+#for f in best_raw_features:
+   # sj_feature_train[f] = pre_process.fit_transform(pd.DataFrame(train_sj[f]))
+    
+    corr = sj_feature_train.corr()
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+fig, ax = plt.subplots(figsize = (15,15))
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+sns.heatmap(data = corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5});
+
+
+
+from sklearn.ensemble import RandomForestRegressor
+mod = RandomForestRegressor()
+
+X = sj_feature_train.drop(['total_cases'], axis = 1).dropna()
+y = sj_feature_train.dropna()['total_cases']
+mod.fit(X,y)
+
+importance = mod.feature_importances_
+importance = pd.DataFrame(importance, index=X.columns, 
+                          columns=["Importance"])
+
+importance["Std"] = np.std([tree.feature_importances_
+                            for tree in mod.estimators_], axis=0)
+
+importance = importance.sort_values(by = 'Importance')
+
+x = range(importance.shape[0])
+y = importance.ix[:, 0]
+yerr = importance.ix[:, 1]
+
+fig, ax = plt.subplots(figsize = (20,10));
+plt.bar(x, y, align="center");
+ax.set_xticks(np.arange(len(importance)));
+ax.set_xticklabels(importance.index.tolist());
+plt.xticks(rotation = 85);
+plt.title('Feature Importance for City sj');
+
+importance.sort_values(by = 'Importance', ascending = False).cumsum()[:20]
---
+++ archive/Model_Benchmarking_archive.py
@@ -0,0 +1,50 @@
+from sklearn.feature_selection import RFECV
+estimator = SVR(kernel="linear")
+sj_selector = RFECV(estimator, step=1, cv=2, n_jobs = -1)
+sj_selector = selector.fit(X_tr_sj, Y_tr_sj)
+sj_selector.support_ 
+
+selector.ranking_
+
+from sklearn.feature_selection import RFECV
+estimator = SVR(kernel="linear")
+iq_selector = RFECV(estimator, step=1, cv=2, n_jobs = -1, verbose = 1)
+iq_selector = selector.fit(X_tr_iq, Y_tr_iq)
+iq_selector.support_ 
+
+
+from sklearn.feature_selection import f_regression
+sj_selector = f_regression(X_tr_sj, Y_tr_sj)
+sj_p_val_pass = 1e-1
+
+X_tr_sj_sel = X_tr_sj.loc[:,sj_selector[1]<sj_p_val_pass]
+X_tr_sj_sel.shape
+
+svrmodels = { 
+    'SVR': SVR()
+}
+
+svrparams = {  
+    'SVR': [
+        {'kernel': ['linear'], 'C': [0.05, 0.01], 'degree':[1], 'epsilon':[0.2, 0.5, 1]}]
+    
+}
+
+svr_sj_helper = EstimatorSelectionHelper(svrmodels, svrparams)
+svr_sj_helper.fit(X_tr_sj_sel, Y_tr_sj, scoring='neg_mean_absolute_error', cv = 2)
+
+svr_sj_helper.score_summary(sort_by='mean_score')
+
+# iq feature selector
+
+from sklearn.feature_selection import f_regression
+iq_selector = f_regression(X_tr_iq, Y_tr_iq)
+iq_p_val_pass = 1e-1
+
+X_tr_iq_sel = X_tr_iq.loc[:,iq_selector[1]<p_val_pass]
+X_tr_iq_sel.shape
+
+svr_iq_helper = EstimatorSelectionHelper(svrmodels, svrparams)
+svr_iq_helper.fit(X_tr_iq_sel, Y_tr_iq, scoring='neg_mean_absolute_error', cv = 2)
+
+svr_iq_helper.score_summary(sort_by='min_score')
